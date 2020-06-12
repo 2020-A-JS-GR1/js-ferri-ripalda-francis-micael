567bbbaa83f8fe2a7866c1fe7c53f50562518bbe
Francis Ferri <francis.ferri@epn.edu.ec>
2020-06-12 17:38:30 -0500

    Truly y Falsy

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -18,9 +18,55 @@ const texto = "Francis"; // string
 const booleano = false; // boolean
 const nulos = null; // object
 const zapatos = undefined; /// undefined
+const apellido = 'Ferri';
 console.log(typeof numero);
 console.log(typeof sueldo);
 console.log(typeof texto);
 console.log(typeof booleano);
 console.log(typeof nulos);
 console.log(typeof zapatos);
+console.log(typeof apellido);
+console.log(typeof Number("asd"));
+console.log(Number("asd"));
+
+if (true == true){
+    console.log("Es Verdadero"); //!
+} else {
+    console.log("Es Falso");
+}
+
+if (true == false){
+    console.log("Es Verdadero");
+} else {
+    console.log("Es Falso");//!
+}
+
+if (""){
+    console.log("Es Verdadero");
+} else {
+    console.log("Es Falso"); //!
+}
+
+if ("Francis"){
+    console.log("Es Verdadero");//!
+} else {
+    console.log("Es Falso");
+}
+
+if (-1){
+    console.log("Es Verdadero"); //!
+} else {
+    console.log("Es Falso");
+}
+
+if (0){
+    console.log("Es Verdadero");
+} else {
+    console.log("Es Falso"); //!
+}
+
+if (1){
+    console.log("Es Verdadero"); //!
+} else {
+    console.log("Es Falso");
+}
