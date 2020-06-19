840c8517f4bb83dee401b042f7f9497499a6d0b1
Francis Ferri <francis.ferri@epn.edu.ec>
2020-06-19 16:55:04 -0500

    Arreglos y empezando operadores

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -11,3 +11,33 @@ cualquierCosa = {};
 console.log(cualquierCosa)
 
 const arregloTodo = [true, 1, 1.2, "Francis", undefined, null, {}, [1, 2, true, "A"]];
+
+// for of
+for (let numero of arreglo){ // valores
+    console.log("numero", numero);
+}
+// for in
+
+for (let indice in arreglo){ // indices
+    console.log("numero", indice);
+}
+
+arreglo.push(11); //Añadir al final un elemento
+console.log(arreglo);
+
+arreglo.pop(); // eliminar al final del arreglo
+console.log(arreglo);
+
+arreglo.unshift(5); // añdir al principio del arreglo
+console.log(arreglo);
+
+arreglo.splice(0,1);
+console.log(arreglo);
+
+arreglo.splice(0,0, 3,4,5);
+
+console.log(arreglo);
+const indice = arreglo.indexOf(9);
+console.log(indice);
+arreglo.splice(indice,1);
+console.log(arreglo);
