c863d50c81b76362a1df2eae82d77240d1d29e3b
Francis Ferri <francis.ferri@epn.edu.ec>
2020-06-26 16:46:06 -0500

    Destructuracion

---
+++ 01-javascript/05-destructuracion.js
@@ -0,0 +1,37 @@
+const adrian = {
+    nombre: "Adrian"
+};
+const carolina = {
+    nombre: "Carolina",
+    apellido: "Eguez"
+};
+
+const adrianCarolina = {
+    ...adrian,
+    ...carolina
+};
+
+adrianCarolina.nombre = "Carolina";
+console.log("adrianCarolina",adrianCarolina);
+console.log("Adrian",adrian);
+
+const arregloUno = [1,2,3,4,5];
+const arregloDos = [6,7,8,9,10];
+
+const superArreglo = {
+    ...arregloUno,
+    ...arregloDos
+}
+
+const superArreglo1 = [
+    ...arregloUno,
+    ...arregloDos
+]
+
+superArreglo[0] = 100;
+console.log("superArreglo", superArreglo);
+console.log("arregloUno", arregloUno);
+
+superArreglo1[0] = 100;
+console.log("superArreglo", superArreglo1);
+console.log("arregloUno", arregloUno);
