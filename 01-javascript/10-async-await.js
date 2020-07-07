8eeb1af728d40138f2b58619aa354b010e68b7a8
Francis Ferri <francis.ferri@epn.edu.ec>
2020-07-07 08:28:14 -0500

    Ejercicio Async Await

--- 01-javascript/10-async-await.js
+++ 01-javascript/10-async-await.js
@@ -1,14 +1,43 @@
-const promesaLeerArchivo = new Promise();
-const promesaEscribirArchivo = new Promise();
+const fs = require("fs");
+
+//const promesaLeerArchivo = new Promise();
+//const promesaEscribirArchivo = new Promise();
 
 // ASYNC AWAIT _> DENTRO DE UNA FUNCION
 
-async function ejercicio() {
+function promesaLeerArchivo(path) {
+    const promesaLectura = new Promise((resolve, reject) => {
+        contenidoArchivo =  fs.readFile(path, "utf-8", (error, contenido) => {
+            if (error) {
+                reject("No se pudo leer el archivo");
+            } else {
+                resolve(contenido);
+            }
+        })
+    });
+    return promesaLectura;
+}
+
+function promesaEscribirArchivo(contenidoNuevo, path){
+    const promesaEscribir = new Promise((resolve, reject) =>{
+        fs.writeFile(path, contenidoNuevo, "utf-8", (error) => {
+            if (error) {
+                reject("No se pudo escribir en el archivo");
+            } else {
+                resolve();
+            }
+        })
+    });
+    return promesaEscribir;
+}
+
+async function ejercicio(path, contenidoNuevo) {
     console.log("1");
     try{
-        const respuestaLecturaArchivo = await promesaLeerArchivo();
-        await  promesaEscribirArchivo();
-        const nuevoContenido = await promesaLeerArchivo();
+        const respuestaLecturaArchivo = await promesaLeerArchivo(path);
+        contenidoNuevo = respuestaLecturaArchivo + "\n" + contenidoNuevo;
+        await  promesaEscribirArchivo(contenidoNuevo, path);
+        const nuevoContenido = await promesaLeerArchivo(path);
         console.log(nuevoContenido);
     } catch(error){
         console.log("error", error)
@@ -18,3 +47,7 @@ async function ejercicio() {
 const f = async () => {
     // contenido
 }
+
+
+
+ejercicio("./06-ejemplo.txt", "Batman");
